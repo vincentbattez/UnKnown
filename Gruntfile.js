a8912318567965ebe11de4d37962bc715f17cd46
vincent battez <vincent.battez.pro@gmail.com>
2017-09-16 17:20:30 +0200

    add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        exec:{
+            prepare:{
+                command:"cordova prepare",
+                stdout:true,
+                stderror:true
+            }
+        },
+        watch:{
+            files:['www/**/*.*'],
+            tasks:['exec:prepare'],
+            options: {
+                livereload: true,
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('default', ['watch']);
+
+};
